5adf3ee8cc890f3c6ae2b577f7d50e9db341cb79
bia1003 <meu.contato.bianca.andrade@gmail.com>
2025-02-26 14:48:49 -0300

    Upload do códigos da primeira aula

---
+++ Aula01/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Aula01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade = 25;
+            string nomeCompleto = "Bianca dos Santos de Andrade";
+
+            var variavelSemTipo = "Bianca";
+            Console.WriteLine(variavelSemTipo.GetType());
+
+            Console.WriteLine("Digite o seu nome: ");
+
+            string nomeUsuario = 
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
